c0b6f11b99b16943189ed81f0da3639b9829e08e
huangguangda <2397923107@qq.com>
2018-03-26 17:15:58 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/calculatordemo/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.calculatordemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
